f21a8d10145bdbf86be2cf4bc880ed1cadd5068d
Kleb-0 <shadowhilles@gmail.com>
2025-11-19 17:56:28 -0300

    feat: add blogs hooks for improved maintainability

---
+++ src/hooks/useBlogs.tsx
@@ -0,0 +1,24 @@
+import Blog from "@/types/blog";
+import useFetch from "./useFetch";
+
+const API_URL = "https://sineum-backend-production.up.railway.app";
+
+export function useBlogs() {
+  const { data, isLoading, error } = useFetch<Blog[]>(`${API_URL}/blogs`);
+
+  return {
+    blogs: data || [],
+    isLoading,
+    error,
+  };
+}
+
+export function useBlog(id: number) {
+  const { data, isLoading, error } = useFetch<Blog>(`${API_URL}/blogs/${id}`);
+
+  return {
+    blog: data,
+    isLoading,
+    error,
+  };
+}
