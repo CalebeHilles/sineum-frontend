dee43d53405d765f2b0c823db940a0ce372b18f0
Kleb-0 <shadowhilles@gmail.com>
2025-11-19 18:07:23 -0300

    feat: add error fallback screen

---
+++ src/components/fallbacks/error.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-2xl">Error</h1>
+      <p>No blog with this name found</p>
+    </div>
+  );
+}
