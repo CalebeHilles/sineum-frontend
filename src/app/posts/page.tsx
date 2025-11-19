6dad8d05c0b021dfcfd096e2e4c1473cfdbadb28
Kleb-0 <shadowhilles@gmail.com>
2025-11-19 18:41:51 -0300

    feat: add dynamic blog cards renderization

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,25 @@
+"use client";
 import Card from "@/components/card";
+import Error from "@/components/fallbacks/error";
+import Loading from "@/components/fallbacks/loading";
+import { useBlogs } from "@/hooks/useBlogs";
 
 export default function Posts() {
+  const { blogs, isLoading, error } = useBlogs();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 gap-3">
-      <Card/>
-      <Card/>
-      <Card/>
+      {blogs.map((blog) => (
+        <Card key={blog.id} />
+      ))}
     </div>
   );
 }
