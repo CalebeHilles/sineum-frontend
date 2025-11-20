1b5e22e07bdc88e9391fce47e2f3fa745ed9104e
Kleb-0 <shadowhilles@gmail.com>
2025-11-19 21:26:39 -0300

    feat: add navigation between Posts and Post pages

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,7 @@ import Card from "@/components/card";
 import Error from "@/components/fallbacks/error";
 import Loading from "@/components/fallbacks/loading";
 import { useBlogs } from "@/hooks/useBlogs";
+import Link from "next/link";
 
 export default function Posts() {
   const { blogs, isLoading, error } = useBlogs();
@@ -18,7 +19,9 @@ export default function Posts() {
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 gap-3">
       {blogs.map((blog) => (
-        <Card title={blog.title} description={blog.description} key={blog.id} />
+        <Link href={"/posts/" + blog.id} key={blog.id}>
+          <Card title={blog.title} description={blog.description} />
+        </Link>
       ))}
     </div>
   );
