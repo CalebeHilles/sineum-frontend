176b6c38d27bebc0f2d66b5e06fc50b77d1e55cc
Kleb-0 <shadowhilles@gmail.com>
2025-12-28 10:09:25 -0300

    style: enhance error component layout for better visibility

--- src/components/fallbacks/error.tsx
+++ src/components/fallbacks/error.tsx
@@ -1,6 +1,6 @@
 export default function Error({ errorMessage }: { errorMessage: string }) {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center items-center">
       <h1 className="text-2xl">Error</h1>
       <p>{errorMessage}</p>
     </div>
